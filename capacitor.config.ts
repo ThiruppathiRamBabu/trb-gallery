ff169040efe67bc27a9a7626ee27c11493b7c06d
ThiruppathiRamBabu <snthiru2000@gmail.com>
2023-07-10 15:57:01 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TRB Gallery',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
